efd4088ff1b982e4ce15e6b04295fe6303cac727
Fernando Higor <ferdshigor@gmail.com>
2023-11-09 17:56:22 -0300

    projeto finalizado

--- src/script/backup.js
+++ src/script/backup.js
@@ -8,37 +8,48 @@ const state = {
         restart: document.querySelector("#button"),
     },
     values: {
-        timerId: null,
         gameVelocity: 1000,
-        countDownTimerId: setInterval(countDown, 1000),
         hitPosition: 0,
         result: 0,
         currentTime: 10,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
     }
 }
+
 function countDown() {
     state.values.currentTime--;
     state.view.timeleft.textContent = state.values.currentTime;
     if (state.values.currentTime <= 0) {
         state.view.gameOver.style.display = "flex";
-        state.values.currentTime = 0;
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+
     }
 }
+
 function restartHitbox() {
     state.view.restart.addEventListener("click", function () {
         state.view.gameOver.style.display = "none";
         state.values.currentTime = 10;
-        state.values.result = 0;
-        state.view.score.textContent = 0;
+
+        setInterval(countDown, 1000),
+            setInterval(randomSquare, 1000);
         init();
 
     });
 }
+
+
+
 function playSound(audioName) {
     let audio = new Audio(`./src/audios/${audioName}.m4a`);
     audio.volume = 0.2;
     audio.play();
 }
+
 function randomSquare() {
     state.view.squares.forEach((square) => {
         square.classList.remove("enemy");
@@ -47,14 +58,14 @@ function randomSquare() {
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
     state.values.hitPosition = randomSquare.id;
-    if (state.values.currentTime === 1) {
-        clearInterval(state.values.timerId);
-    }
+
 }
-function moveEnemy(velocidade) {
-    clearInterval(state.values.timerId);
-    state.values.timerId = setInterval(randomSquare, velocidade);
-}
+
+/*function moveEnemy() {
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+
+}*/
+
 function addListenerHitBox() {
     state.view.squares.forEach((square) => {
         square.addEventListener("mousedown", () => {
@@ -67,9 +78,13 @@ function addListenerHitBox() {
         })
     })
 }
+
 function init() {
-    moveEnemy(state.values.gameVelocity);
+    // moveEnemy();
     addListenerHitBox();
     restartHitbox();
+
+
 }
+
 init();
